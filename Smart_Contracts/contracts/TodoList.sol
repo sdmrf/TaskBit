74344e908d6af6db963ef22f15438f5da6baae1a
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-24 20:30:03 +0530

    First SmartContract Created

--- Smart_Contracts/contracts/TodoList.sol
+++ Smart_Contracts/contracts/TodoList.sol
@@ -4,4 +4,21 @@ pragma solidity >=0.5 <0.9.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Developed by: Sdmrf");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
